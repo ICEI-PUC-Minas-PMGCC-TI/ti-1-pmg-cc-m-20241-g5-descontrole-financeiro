3f40b8dc579b8e1e26b21b43566402cf47a8d665
João Marcos <163580854+Darsoot@users.noreply.github.com>
2024-05-19 15:15:46 -0300

    Analisa a resposta

--- codigo/Fases/B.js
+++ codigo/Fases/B.js
@@ -1,13 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const respostaArmazenada = sessionStorage.getItem('resposta');
-    const feedbackResposta = document.getElementById('feedbackResposta');
-    if (respostaArmazenada === 'correta') {
-        feedbackResposta.textContent = 'correta';
-        feedbackResposta.style.color = 'green';
-    } else if (respostaArmazenada === 'incorreta') {
-        feedbackResposta.textContent = 'incorreta';
-        feedbackResposta.style.color = 'red';
-    } else {
-        feedbackResposta.textContent = 'Não foi possível determinar a resposta.';
-    }
+    const continuarBtn = document.querySelector('.btn_continuar');
+    const options = document.querySelectorAll('.perg input[type="radio"]');
+    const respostaDiv = document.getElementById('resposta');
+    const respostaCorreta = document.getElementById('respostaCorreta');
+
+    continuarBtn.disabled = true;
+    continuarBtn.classList.add('botao-inativo');
+
+    options.forEach(option => {
+        option.addEventListener('change', function() {
+            continuarBtn.disabled = false;
+            continuarBtn.classList.remove('botao-inativo');
+        });
+    });
+
+    continuarBtn.addEventListener('click', function() {
+        const respostaSelecionada = document.querySelector('.perg input[type="radio"]:checked');
+        if (respostaSelecionada) {
+            const isCorreta = respostaSelecionada.parentElement.classList.contains('resposta-correta');
+            if (isCorreta) {
+                respostaCorreta.textContent = "correta";
+                respostaDiv.style.color = 'green';
+            } else {
+                respostaCorreta.textContent = "incorreta";
+                respostaDiv.style.color = 'red';
+            }
+            respostaDiv.style.display = 'block';
+            continuarBtn.disabled = true;
+            continuarBtn.classList.add('botao-inativo');
+        }
+    });
 });
