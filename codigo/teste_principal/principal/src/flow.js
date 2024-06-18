4141356b4eb78ef534a0f7d7ef7baeab33488fb2
ArthurSGA <arodrigues@sga.pucminas.br>
2024-06-18 13:37:42 -0300

    Progressão na funcionalidade das Questões Depois de ter conseguido identificar a fase e o capítulo de cada botão no mapa de fases. Fiz a comunicação entre páginas por meio de Query Strings. A comunicação está funcional, agora, falta apenas mostrar o conteúdo pertinente aos dados fornecidos, o que já está em andamento.

--- codigo/teste_principal/principal/src/flow.js
+++ codigo/teste_principal/principal/src/flow.js
@@ -1,14 +1,20 @@
+
+
+
+
 function logAnchorIds() {
     const anchorElements = document.querySelectorAll('.fase a');  // Select all anchors within elements with class "fase"
   
     anchorElements.forEach(anchor => {
       anchor.addEventListener('click', function() {
         console.log(`This is chapter ${anchor.id[0]} and phase ${anchor.id[1]}`);
-        console.log(Number(anchor.id[0])+Number(anchor.id[1]));
+        const url = `../questao.html?chapter=${anchor.id[0]}&phase=${anchor.id[1]}`;
+            // Redirect to the URL
+        window.location.href = url;
       })
     });
   }
   
   // Call the function after the elements are potentially loaded
-  
-setTimeout(logAnchorIds, 1000); // Add a slight delay to ensure elements are loaded
+
+setTimeout(logAnchorIds, 1000); 
